13faa5b547b0af66ecdeb98810e96d00baaa7002
ElDainosor <eldainosor@gmail.com>
2018-11-11 14:16:10 +0530

    QSTile: Add Squaremedo from BootLeggers [3/3]

--- config/common.mk
+++ config/common.mk
@@ -106,7 +106,8 @@ PRODUCT_PACKAGES += \
     QStileDottedCircle \
     QStileNinja \
     QStilePokesign \
-    QStileWavey
+    QStileWavey \
+    QStileSquaremedo
 
 # Omni's PoorMan Themes
 PRODUCT_PACKAGES += \
