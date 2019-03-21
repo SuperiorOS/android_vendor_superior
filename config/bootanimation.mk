a16df9ffa7fd2009bc33f79e0ba57f529189ef3c
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2019-03-21 16:57:08 +0530

    vendor: Add more bootanimation variants
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Ide2a9372193720ed38c7e2bd182776e38d5db43b

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -13,10 +13,14 @@
 # limitations under the License.
 
 # Bootanimation
-ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+ifeq ($(TARGET_BOOT_ANIMATION_RES),480)
+     PRODUCT_COPY_FILES += vendor/superior/prebuilt/common/bootanimation/bootanimation-480p.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
      PRODUCT_COPY_FILES += vendor/superior/prebuilt/common/bootanimation/bootanimation-720p.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
      PRODUCT_COPY_FILES += vendor/superior/prebuilt/common/bootanimation/bootanimation-1080p.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/superior/prebuilt/common/bootanimation/bootanimation-1440p.zip:system/media/bootanimation.zip
 else
      $(warning TARGET_BOOT_ANIMATION_RES is invalid or undefined, using generic bootanimation)
 PRODUCT_COPY_FILES += \
