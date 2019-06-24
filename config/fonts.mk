a3e1b593449821b67e67af49e524b51da01ca828
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2019-06-25 01:48:23 +0530

    fonts: clean up & Import BTLG fonts [3/3]
    Co-authored-by: ElDainosor <eldainosor@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I74ea18d39e9c789df68c9b4e89211e0bcda2f9df

--- config/fonts.mk
+++ config/fonts.mk
@@ -19,19 +19,36 @@ PRODUCT_COPY_FILES += \
     vendor/superior/prebuilt/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
     vendor/superior/prebuilt/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
     vendor/superior/prebuilt/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf \
-    vendor/superior/prebuilt/fonts/neoneon/neoneon.ttf:system/fonts/Neoneon-Regular.ttf \
     vendor/superior/prebuilt/fonts/roadrage/road_rage.ttf:system/fonts/RoadRage-Regular.ttf \
     vendor/superior/prebuilt/fonts/snowstorm/snowstorm.ttf:system/fonts/Snowstorm-Regular.ttf \
     vendor/superior/prebuilt/fonts/vcrosd/vcr_osd_mono.ttf:system/fonts/ThemeableFont-Regular.ttf \
-    vendor/superior/prebuilt/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf \
+    vendor/superior/prebuilt/fonts/vcrosd/vcr_osd_mono.ttf:system/fonts/ThemeableClock-vcrosd.ttf \
     vendor/superior/prebuilt/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
     vendor/superior/prebuilt/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
     vendor/superior/prebuilt/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
     vendor/superior/prebuilt/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
     vendor/superior/prebuilt/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
     vendor/superior/prebuilt/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf \
-    vendor/superior/prebuilt/fonts/ABCThru/ABCThru.ttf:system/fonts/ABCThru.ttf \
     vendor/superior/prebuilt/fonts/anurati/anurati_regular.ttf:system/fonts/anurati_regular.ttf \
     vendor/superior/prebuilt/fonts/Joost/JoostAMillionaireNF.ttf:system/fonts/JoostAMillionaireNF.ttf \
-    vendor/superior/prebuilt/fonts/Locust/Locust_Resistance.ttf:system/fonts/Locust_Resistance.ttf \
-    vendor/superior/prebuilt/fonts/Wallpoet/Wallpoet_regular.ttf:system/fonts/Wallpoet_regular.ttf
+    vendor/superior/prebuilt/fonts/BTLG/AlienLeague-Regular.ttf:system/fonts/AlienLeague-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/BigNoodleTilting-Italic.ttf:system/fonts/BigNoodleTilting-Italic.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/BigNoodleTilting-Regular.ttf:system/fonts/BigNoodleTilting-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/Biko-Regular.ttf:system/fonts/Biko-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/Blern-Regular.ttf:system/fonts/Blern-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/GinoraSans-Regular.ttf:system/fonts/GinoraSans-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/Inkferno-Regular.ttf:system/fonts/Inkferno-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/Metropolis1920-Regular.ttf:system/fonts/Metropolis1920-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/Neoneon-Regular.ttf:system/fonts/Neoneon-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/Riviera-Regular.ttf:system/fonts/Riviera-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/Abel-Regular.ttf:system/fonts/Abel-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/AdventPro-Regular.ttf:system/fonts/AdventPro-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/CherrySwash-Regular.ttf:system/fonts/CherrySwash-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/Codystar.ttf:system/fonts/Codystar.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/Jura-Regular.ttf:system/fonts/Jura-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/KellySlab-Regular.ttf:system/fonts/KellySlab-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/Pompiere-Regular.ttf:system/fonts/Pompiere-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/ReemKufi-Regular.ttf:system/fonts/ReemKufi-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/SedgwickAveDisplay-Regular.ttf:system/fonts/SedgwickAveDisplay-Regular.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/Vibur.ttf:system/fonts/Vibur.ttf \
+    vendor/superior/prebuilt/fonts/BTLG/Voltaire.ttf:system/fonts/Voltaire.ttf
