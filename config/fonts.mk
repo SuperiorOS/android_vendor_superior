50418c3879b0a73c53341b4da8e10fb4d715e530
Alex <du.alexcruz@gmail.com>
2020-01-17 22:00:55 -0500

    Fonts: Import fonts.xml from fwb
    Commit history can be found here
    https://github.com/DirtyUnicorns/android_frameworks_base/commits/q10x/data/fonts/fonts.xml
    
    Change-Id: Iae43a32ae2d3aa480af07508fd5e71801269a336
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,23 +1,3 @@
-# Copyright (C) 2018-19 The Superior OS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Google Fonts
-PRODUCT_COPY_FILES += \
-    vendor/superior/prebuilt/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
-    vendor/superior/prebuilt/fonts/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Medium.ttf \
-    vendor/superior/prebuilt/fonts/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-MediumItalic.ttf \
-    vendor/superior/prebuilt/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
-    vendor/superior/prebuilt/fonts/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Bold.ttf \
-    vendor/superior/prebuilt/fonts/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-BoldItalic.ttf \
-    vendor/superior/prebuilt/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+# Fonts
+PRODUCT_PACKAGES := \
+    fonts.xml
