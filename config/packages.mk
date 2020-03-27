b2aa75e80c3028084a9f2cb5227c88fc8265cb09
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-03-27 16:55:56 -0300

    vendor: Port face unlock feature [3/3]
    - Build face unlock for arm64 devices
    - Also add a flag to opt-out
    
    Change-Id: I9d4bb7174d28ca067665893a8096b3176a5aa7a6
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -109,8 +109,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
-# Launchers
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
 
+# Launchers
 ifeq ($(TARGET_LAUNCHER),lawnchair)
 include vendor/superior/prebuilt/app/Lawnchair/lawnchair.mk
 else ifeq ($(TARGET_LAUNCHER),oplauncher)
