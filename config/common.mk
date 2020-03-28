3c91faea142e5664bd64b1f5ffd91a03bb388712
Jayant Deshmukh <jayantdeshmuk008@gmail.com>
2020-03-28 09:21:42 +0000

    vendor: copy some files permissions to system instead of system/product
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Ib409cd08ed8866f123763bc14c8059278ca2e041

--- config/common.mk
+++ config/common.mk
@@ -57,7 +57,8 @@ PRODUCT_COPY_FILES += \
 #Superior Permissions
 PRODUCT_COPY_FILES += \
     vendor/superior/config/permissions/privapp-permissions-superior-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-superior.xml \
-    vendor/superior/config/permissions/superior-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/superior-hiddenapi-package-whitelist.xml
+    vendor/superior/config/permissions/superior-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/superior-hiddenapi-package-whitelist.xml \
+    vendor/superior/config/permissions/privapp-permissions-superior.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-superior.xml
 
 # Include LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/superior/overlay/dictionaries
