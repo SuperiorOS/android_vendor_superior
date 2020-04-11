f196dc9d01dd6b84d9d23e7778d00acdd415e444
Henrique Silva <jhenrique09.mcz@hotmail.com>
2020-04-11 18:17:38 +0530

    vendor: Add qcom platform type to soongs vars
    Required for new commits on frameworks/native
    
    * Fix lag during screen recording on some platforms
    * Fix screen flickering after disconnecting from Android Auto on some platforms
    
    Change-Id: Ia0ed0bbfda44fb5ccb184ace854ec1bf9a7d819a
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -56,6 +56,31 @@ type Product_variables struct {
 		Cppflags []string
 		Shared_libs []string
 	}
+	Uses_qcom_um_family struct {
+		Cflags []string
+		Srcs []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_3_18_family struct {
+		Include_dirs []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_4_4_family struct {
+		Include_dirs []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_4_9_family struct {
+		Include_dirs []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_4_14_family struct {
+		Include_dirs []string
+		Header_libs []string
+		Shared_libs []string
+	}
 }
 
 type ProductVariables struct {
@@ -77,4 +102,9 @@ type ProductVariables struct {
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
 	Uses_qti_camera_device  *bool `json:",omitempty"`
+	Uses_qcom_um_family					*bool `json:",omitempty"`
+	Uses_qcom_um_3_18_family				*bool `json:",omitempty"`
+	Uses_qcom_um_4_4_family					*bool `json:",omitempty"`
+	Uses_qcom_um_4_9_family					*bool `json:",omitempty"`
+	Uses_qcom_um_4_14_family				*bool `json:",omitempty"`
 }
