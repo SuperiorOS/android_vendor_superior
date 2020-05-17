d1a88cd0b2dcc2878a366457638c756279b204ab
Dyneteve <dyneteve@pixelexperience.org>
2020-05-17 23:19:03 +0530

    vendor: Fix compilation errors with prebuilt kernel
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>
    Change-Id: I847b59d3e3e1a04438d827b7db804f5ce3cf53a3
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -53,6 +53,8 @@ KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
 endif
 
 GCC_PREBUILTS := $(BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86
+TARGET_KERNEL_HEADERS ?= $(TARGET_KERNEL_SOURCE)
+
 # arm64 toolchain
 KERNEL_TOOLCHAIN_arm64 := $(GCC_PREBUILTS)/aarch64/aarch64-linux-android-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX_arm64 := aarch64-linux-android-
