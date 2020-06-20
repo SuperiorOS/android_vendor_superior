253497be61bea1d2112f60f31e0c4b9d59c040ae
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2020-06-20 19:30:05 +0530

    Revert "vendor: Allow providing camera server and service"
    This reverts commit b0e3f6996dcfd7bba4708a4a349c2c0e9680d745.
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I16a59de6a872775e7001f9d8bcfe033f33a8b6d0

--- config/BoardConfigSuperior.mk
+++ config/BoardConfigSuperior.mk
@@ -5,9 +5,3 @@ include vendor/superior/config/BoardConfigQcom.mk
 endif
 
 include vendor/superior/config/BoardConfigSoong.mk
-
-ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true)
-PRODUCT_SOONG_NAMESPACES += \
-    frameworks/av/camera/cameraserver \
-    frameworks/av/services/camera/libcameraservice
-endif
