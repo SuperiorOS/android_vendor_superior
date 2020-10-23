DirtyUnicorns/android_vendor_du

Emulator: Add pixel launcher / SystemUI permissions
This makes it boot again since the Emulator doesn't ship with gapps

Change-Id: Iddf243d571ebb0b45231accf478b02336343b5cd

config/common.mk
@@ -34,6 +34,10 @@ DU_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/du/prebuilt/common/etc/dirtyunicorns.rc:system/etc/init/dirtyunicorns.rc
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/du/prebuilt/common/etc/custom_permissions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/custom_permissions.xml
+
 # Backup tool
 PRODUCT_COPY_FILES += \
     vendor/extras/build/tools/backuptool.sh:install/bin/backuptool.sh \

SuperiorOS/android_vendor_superior

Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

config/common.mk
@@ -62,6 +62,10 @@ PRODUCT_COPY_FILES += \
     vendor/superior/config/permissions/privapp-permissions-superior-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-superior.xml \
     vendor/superior/config/permissions/privapp-permissions-superior.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-superior.xml
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/superior/prebuilt/common/etc/custom_permissions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/custom_permissions.xml
+
 # Include LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/superior/overlay/dictionaries
 