759862a4980d5ff1bb5464870eb03d122db1f652
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2020-12-21 19:01:44 +0000

    Revert safetynet hacks!
    Revert "vendor: Force latest redfin build fingerprint"
    
    This reverts commit c5ad7b6fe2aad8284c10b24b7313ad8c71da6e7d.
    
    Revert "vendor: Allow forcing custom build fingerprint [1/2]"
    
    This reverts commit 5d3da39f548937fc79d554a7f59d35d244893c76.
    
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Change-Id: Ia19cb33a9f2b966acbfb81050cf4eee07ba32d16

--- config/BoardConfigSuperior.mk
+++ config/BoardConfigSuperior.mk
@@ -1,6 +1,3 @@
-# Safetynet
-TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RD1A.201105.003.C1/6886399:user/release-keys
-
 include vendor/superior/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
