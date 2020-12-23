Project-LegionOS/vendor_legion

copy backuptool files to system
For some reason in android11 it fails to copy to $OUT/install, so copy to system as a temporary workaround till the root cause is discovered

config/common.mk
@@ -20,8 +20,8 @@ endif
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/legion/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/legion/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/legion/prebuilt/common/bin/backuptool.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.sh \
+    vendor/legion/prebuilt/common/bin/backuptool.functions:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/legion/prebuilt/common/bin/50-legion.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-legion.sh
 
 ifneq ($(AB_OTA_PARTITIONS),)

SuperiorOS/android_vendor_superior

Change-Id: I417d9b021a4bb89eac64db57f3d2210b661723c3

config/common.mk
@@ -16,8 +16,8 @@ PRODUCT_BRAND ?= SuperiorOS
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/superior/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/superior/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/superior/prebuilt/common/bin/backuptool.sh:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.sh \
+    vendor/superior/prebuilt/common/bin/backuptool.functions:$(TARGET_COPY_OUT_SYSTEM)/install/bin/backuptool.functions \
     vendor/superior/prebuilt/common/bin/50-superior.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-superior.sh
 
 ifneq ($(AB_OTA_PARTITIONS),)