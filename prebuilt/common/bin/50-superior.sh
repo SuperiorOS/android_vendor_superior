f85c5d275625763c62758435cc612ec10914baf8
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2021-06-10 21:19:34 +0530

    vendor: Fixup backuptool
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Ied7a1b4562f53f24c17ecea4de1819fcfd8b0f1c

--- prebuilt/common/bin/50-superior.sh
+++ prebuilt/common/bin/50-superior.sh
@@ -2,8 +2,8 @@
 #
 # ADDOND_VERSION=2
 #
-# /system/addon.d/50-lineage.sh
-# During a LineageOS upgrade, this script backs up /system/etc/hosts,
+# /system/addon.d/50-superior.sh
+# During a SuperiorOS upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
 #
 
