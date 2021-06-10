993b7cfffda564c67b8702f0acac18beacad9953
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2021-06-10 21:19:34 +0530

    vendor: Fixup backuptool
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Ied7a1b4562f53f24c17ecea4de1819fcfd8b0f1c

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -6,7 +6,6 @@
 export C=/tmp/backupdir
 export SYSDEV="$(readlink -nf "$2")"
 export SYSFS="$3"
-export V=11
 
 export ADDOND_VERSION=3
 
@@ -46,10 +45,6 @@ check_prereq() {
 if [ ! -r $S/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
-fi
-if ! grep -q "^ro.build.version.release=$V.*" $S/build.prop; then
-  echo "Backup/restore is not possible. Incompatible ROM version: $V"
-  return 2
 fi
 return 0
 }
--- prebuilt/common/bin/backuptool_postinstall.sh
+++ prebuilt/common/bin/backuptool_postinstall.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 #
-# LineageOS A/B OTA Postinstall Script
+# A/B OTA Postinstall Script
 #
 
 /postinstall/system/bin/backuptool_ab.sh backup
