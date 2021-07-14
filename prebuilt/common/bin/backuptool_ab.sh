d560268f84dbac548c45d98a130a9fab2832837f
ender1324 <ender1324@abv.bg>
2021-07-14 14:33:48 +0000

    vendor: backuptool: Switch to android version for backuptool
    * Bumping superior version everytime is pain
    
    Change-Id: Ieacc66122f1ef9edb087a9302597a77a884181a1

--- prebuilt/common/bin/backuptool_ab.sh
+++ prebuilt/common/bin/backuptool_ab.sh
@@ -5,7 +5,7 @@
 
 export S=/system
 export C=/postinstall/tmp/backupdir
-export V=20.0
+export V=14
 
 export ADDOND_VERSION=3
 
@@ -56,7 +56,7 @@ if [ ! -r /system/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
 fi
-if ! grep -q "^ro.superior.version=$V.*" /system/build.prop; then
+if ! grep -q "^ro.build.version.release=$V.*" /system/build.prop; then
   echo "Backup/restore is not possible. Incompatible ROM version: $V"
   return 2
 fi
