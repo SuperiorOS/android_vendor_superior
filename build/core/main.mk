Moon-OS/vendor_moon

vendor: Set our custom build fingerprint
Change-Id: I98bace21be7ead9a8bf4fbcde5f316a08e179c88

build/core/main.mk
@@ -1,0 +1,1 @@
+include $(TOPDIR)vendor/aosp/build/core/main_version.mk

build/core/main_version.mk
@@ -1,0 +1,10 @@
+# Build fingerprint
+BUILD_NUMBER_CUSTOM := $(shell date -u +%H%M)
+ifneq ($(filter OFFICIAL,$(CUSTOM_BUILD_TYPE)),)
+BUILD_SIGNATURE_KEYS := release-keys
+else
+BUILD_SIGNATURE_KEYS := test-keys
+endif
+BUILD_FINGERPRINT := $(PRODUCT_BRAND)/$(TARGET_DEVICE)/$(TARGET_DEVICE):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_NUMBER_CUSTOM):$(TARGET_BUILD_VARIANT)/$(BUILD_SIGNATURE_KEYS)
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)

SuperiorOS/android_vendor_superior

build/core/main.mk
@@ -1,0 +1,1 @@
+include $(TOPDIR)vendor/superior/build/core/main_version.mk

build/core/main_version.mk
@@ -1,0 +1,10 @@
+# Build fingerprint
+BUILD_NUMBER_CUSTOM := $(shell date -u +%H%M)
+ifneq ($(filter OFFICIAL,$(SUPERIOR_BUILD_TYPE)),)
+BUILD_SIGNATURE_KEYS := release-keys
+else
+BUILD_SIGNATURE_KEYS := test-keys
+endif
+BUILD_FINGERPRINT := $(PRODUCT_BRAND)/$(TARGET_DEVICE)/$(TARGET_DEVICE):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_NUMBER_CUSTOM):$(TARGET_BUILD_VARIANT)/$(BUILD_SIGNATURE_KEYS)
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)