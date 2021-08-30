2b9545817f3ef743c8f619de42bcf0673f6f17b3
indraadp <indraproject777@gmail.com>
2021-08-30 13:53:52 +0000

    Revert "Revert "vendor: Conditionally enable AOSP blur""
    This reverts commit 732e82430bdc246e59c110a63405e9a7e0f1da22.

--- config/common.mk
+++ config/common.mk
@@ -93,6 +93,13 @@ DEVICE_PACKAGE_OVERLAYS += vendor/superior/overlay/common
 #Telephony
 $(call inherit-product, vendor/superior/config/telephony.mk)
 
+# Blur
+ifeq ($(TARGET_USES_BLUR), true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+endif
+
 # Packages
 include vendor/superior/config/packages.mk
 
