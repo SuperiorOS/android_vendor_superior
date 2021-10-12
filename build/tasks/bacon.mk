2c4c0062a20241aa174f483b605f8a0910adae8d
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-10-12 19:09:29 +0530

    vendor: Add build finished info
    Change-Id: I6b7a45705bb20280ed71a8226fa0d57530c28747
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -20,8 +20,18 @@ SUPERIOR_TARGET_PACKAGE := $(PRODUCT_OUT)/superior-$(SUPERIOR_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
+CL_PRP="\033[35m"
+CL_RED="\033[31m"
+CL_GRN="\033[32m"
+
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(SUPERIOR_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(SUPERIOR_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(SUPERIOR_TARGET_PACKAGE).sha256sum
-	@echo "Package Complete: $(SUPERIOR_TARGET_PACKAGE)" >&2
+	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
+	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(SUPERIOR_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"MD5: "${CL_RED}" `cat $(SUPERIOR_TARGET_PACKAGE).md5sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(SUPERIOR_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"TimeStamp:"${CL_RED}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.build.date.utc | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Integer Value:"${CL_RED}" `wc -c $(SUPERIOR_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
