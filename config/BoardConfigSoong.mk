87ab2ca7c12cb04c095c5141262b362e0b8990fb
Anushek Prasal <anushekprasal@gmail.com>
2022-01-01 10:29:30 +0530

    vendor: soong: Add camera_needs_client_info
    Change-Id: I08fa7ce273d59f171b9e22e74c7ecef5e3c94866
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -31,6 +31,7 @@ SOONG_CONFIG_NAMESPACES += superiorGlobalVars
 SOONG_CONFIG_superiorGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    camera_needs_client_info \
     has_legacy_camera_hal1 \
     ignores_ftp_pptp_conntrack_failure \
     needs_camera_boottime \
@@ -88,6 +89,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_superiorGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_superiorGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_superiorGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
 SOONG_CONFIG_superiorGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_superiorGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
