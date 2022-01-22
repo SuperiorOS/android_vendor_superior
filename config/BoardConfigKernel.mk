699a726ccd7e266821b79032a0773f718781704f
Dakkshesh07 <dakkshesh5@gmail.com>
2022-01-22 22:53:20 +0530

    config: kernel: Add support to use GNU binutils from clang
    Currently GNU binutils like "as" is used from 4.9 gcc.
    Custom Clang compilers like Proton, Neutron, AtomX ship with GNU binutils in them,
    So add support to use the binutils from clang dir directly and remove the use of gcc 4.9
    
    Signed-off-by: Dakkshesh07 <dakkshesh5@gmail.com>
    Change-Id: I9e105f8996943a7aaf892ed44dfff86ae58f24b4

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -87,10 +87,20 @@ else
 endif
 
 CLANG_PREBUILTS := $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(CLANG_PREBUILTS_VERSION)
+
+ifeq ($(TARGET_CLANG_WITH_GNU_BINUTILS),true)
+# arm64 toolchain
+KERNEL_TOOLCHAIN_arm64 := $(CLANG_PREBUILTS)/bin
+KERNEL_TOOLCHAIN_PREFIX_arm64 := aarch64-linux-gnu-
+# arm toolchain
+KERNEL_TOOLCHAIN_arm := $(KERNEL_TOOLCHAIN_arm64)
+KERNEL_TOOLCHAIN_PREFIX_arm := arm-linux-gnueabi-
+else
 GCC_PREBUILTS := $(BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)
 # x86 toolchain
 KERNEL_TOOLCHAIN_x86 := $(GCC_PREBUILTS)/x86/x86_64-linux-android-4.9/bin
 KERNEL_TOOLCHAIN_PREFIX_x86 := x86_64-linux-android-
+endif
 
 ifeq ($(TARGET_KERNEL_NEW_GCC_COMPILE),true)
     ifeq ($(TARGET_KERNEL_CLANG_COMPILE),true)
