27fa1f5c7231059541e5e5dee850380e385691be
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2022-02-14 10:00:19 +0530

    Revert "vendor: Add soong variable for TARGET_CAMERA_BOOTTIME_TIMESTAMP"
    This reverts commit a4f2265b89707a158168a58175549ea36ca03b91.
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I6530f2c1eba23214f21b6d987cf8e35cea7d2c3e

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -55,8 +55,7 @@ SOONG_CONFIG_superiorQcomVars += \
     supports_hw_fde_perf \
     uses_pre_uplink_features_netmgrd \
     uses_qcom_bsp_legacy \
-    uses_qti_camera_device \
-    needs_camera_boottime_timestamp
+    uses_qti_camera_device
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
@@ -78,7 +77,6 @@ SOONG_CONFIG_superiorQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEG
 SOONG_CONFIG_superiorQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 SOONG_CONFIG_superiorGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)
 SOONG_CONFIG_superiorQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
-SOONG_CONFIG_superiorQcomVars_needs_camera_boottime_timestamp := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
