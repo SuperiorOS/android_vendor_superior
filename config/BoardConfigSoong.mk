546734ce2ad498ae10c21cfc1058a0ef13854edf
jhenrique09 <jhsv09@gmail.com>
2022-05-23 21:18:20 +0000

    vendor: Allow using legacy fd
    Change-Id: I88e355e60de63a7f81adf8e25fabbd305fb0af7c
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -51,6 +51,7 @@ SOONG_CONFIG_superiorGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     uses_egl_display_array \
+    uses_legacy_fd_fbdev \
     uses_nothing_camera \
     uses_oplus_camera
 
@@ -80,6 +81,7 @@ SOONG_CONFIG_superiorGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISP
 SOONG_CONFIG_superiorNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_superiorQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_superiorQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_superiorGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
 SOONG_CONFIG_superiorGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_superiorGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 
