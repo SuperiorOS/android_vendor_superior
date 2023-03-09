3c96db04cbaffeab6c3043db554735845cb70ac0
Joker-V2 <nipinna0@gmail.com>
2023-03-09 03:54:08 +0100

    roomservice: Update to thirteen
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -30,8 +30,8 @@ import urllib.request
 DEBUG = False
 
 custom_local_manifest = ".repo/local_manifests/superior.xml"
-custom_default_revision = "twelve"
-custom_default_revision =  os.getenv('ROOMSERVICE_DEFAULT_BRANCH', 'twelve')
+custom_default_revision = "thirteen"
+custom_default_revision =  os.getenv('ROOMSERVICE_DEFAULT_BRANCH', 'thirteen')
 custom_dependencies = "superior.dependencies"
 org_manifest = "SuperiorOS-Devices"  # leave empty if org is provided in manifest
 org_display = "SuperiorOS-Devices"  # needed for displaying
