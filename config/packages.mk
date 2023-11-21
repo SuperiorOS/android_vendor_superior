fa510bcb59b5ae28232e17175cfe72152e1861e0
S I P U N <sipunkumar85@gmail.com>
2023-11-21 19:40:14 +0100

    vendor: charger: build pixel charging animation by default
    reverts ad5f824
    
    Change-Id: I822e6541ef96cd37b8bd5114712681df0f0b8ff5
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -38,12 +38,10 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/superior/overlay/dictionaries
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/superior/overlay/dictionaries
 
 # Charger
-ifeq ($(USE_PIXEL_CHARGER),true)
 PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images \
     product_charger_res_images_vendor
-endif
 
 # Extra tools in Superior
 PRODUCT_PACKAGES += \
