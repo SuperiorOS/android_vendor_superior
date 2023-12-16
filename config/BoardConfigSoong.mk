bananadroid/android_vendor_banana

soong: Add TARGET_INCLUDES_MIUI_CAMERA
- Inspired from 11f751a76380eca5e5bd1b5db329ff9fc4c732df

config/BoardConfigSoong.mk
@@ -45,6 +45,7 @@ SOONG_CONFIG_bananaGlobalVars += \
     uses_oplus_camera \
     uses_oppo_camera \
     uses_nothing_camera \
+    include_miui_camera \
     has_legacy_camera_hal1 \
     has_memfd_backport \
     target_health_charging_control_charging_path \
@@ -92,6 +93,7 @@ SOONG_CONFIG_bananaGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_bananaGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 SOONG_CONFIG_bananaGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_bananaGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
+SOONG_CONFIG_bananaGlobalVars_include_miui_camera := $(TARGET_INCLUDES_MIUI_CAMERA)
 SOONG_CONFIG_bananaGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_bananaGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_bananaGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)

SuperiorOS/android_vendor_superior

Change-Id: I72b74aa5dfcd25cda70f8b754c58fe1af38163db
Signed-off-by: S I P U N <sipunkumar85@gmail.com>

config/BoardConfigSoong.mk
@@ -38,6 +38,7 @@ SOONG_CONFIG_superiorGlobalVars += \
     disable_bluetooth_le_set_host_feature \
     gralloc_handle_has_custom_content_md_reserved_size \
     gralloc_handle_has_reserved_size \
+    include_miui_camera \
     needs_camera_boottime \
     target_alternative_futex_waiters \
     target_health_charging_control_charging_path \
@@ -86,6 +87,7 @@ SOONG_CONFIG_superiorGlobalVars_camera_override_format_from_reserved := $(TARGET
 SOONG_CONFIG_superiorGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
 SOONG_CONFIG_superiorGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_superiorGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
+SOONG_CONFIG_superiorGlobalVars_include_miui_camera := $(TARGET_INCLUDES_MIUI_CAMERA)
 SOONG_CONFIG_superiorGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_superiorNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_superiorQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)