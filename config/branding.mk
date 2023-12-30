fe0d903b66f7c29dd1d40c4a598114daa49db935
Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
2023-12-31 00:06:34 +0530

    vendor: Drop alpha tag
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- config/branding.mk
+++ config/branding.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 #Superior OS Versioning :
-SUPERIOR_MOD_VERSION = Fourteen-Alpha
+SUPERIOR_MOD_VERSION = Fourteen
 
 ifndef SUPERIOR_BUILD_TYPE
     SUPERIOR_BUILD_TYPE := COMMUNITY
