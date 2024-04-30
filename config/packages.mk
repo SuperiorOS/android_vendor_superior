282ae90324a3ba63083722acc2eacf229e74ab95
S I P U N <sipunkumar85@gmail.com>
2024-04-30 17:47:23 +0000

    vendor: config: Build some packages
    - partially reverts https://github.com/SuperiorOS/android_vendor_superior/commit/8c305fef959870b8b534eae1e63dff85dbae3471
    
    Change-Id: I4e4c804c43e4d4b23597b413d3d0549d511ee4c7
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -48,14 +48,45 @@ PRODUCT_PACKAGES += \
 
 # Extra tools in Superior
 PRODUCT_PACKAGES += \
+    bash \
     curl \
     getcap \
-    setcap
+    htop \
+    nano \
+    setcap \
+    vim
 
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/curl \
     system/bin/getcap \
     system/bin/setcap
+
+# Filesystems tools
+PRODUCT_PACKAGES += \
+    fsck.ntfs \
+    mkfs.ntfs \
+    mount.ntfs
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/fsck.ntfs \
+    system/bin/mkfs.ntfs \
+    system/bin/mount.ntfs \
+    system/%/libfuse-lite.so \
+    system/%/libntfs-3g.so
+
+# Openssh
+PRODUCT_PACKAGES += \
+    scp \
+    sftp \
+    ssh \
+    sshd \
+    sshd_config \
+    ssh-keygen \
+    start-ssh
+
+# rsync
+PRODUCT_PACKAGES += \
+    rsync
 
 ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
